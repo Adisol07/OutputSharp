218e155989628dd5d8d43c1570dece95472e6598
Adisol <Adisol@seznam.cz>
2023-01-21 09:56:59 +0100

    First

--- OutputSharp/details.cs
+++ OutputSharp/details.cs
@@ -11,7 +11,7 @@ namespace OutputSharp
         public static string Version { get; } = "1.0.0";
         public static string Snapshot { get; } = "001";
         public static string FullVersion { get; } = Version + "-" + Snapshot;
-        public static string CountVersion { get; } = "2";
+        public static string CountVersion { get; } = "0";
         public static string OneDigitVersion { get; } = "1";
     }
 }
